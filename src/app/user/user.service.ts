ec160274abbedc58e00e2fafd27df933d443d290
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-05 09:00:16 -0600

    Add: impl user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  user = [
+    {id:1, name: 'john doe'},
+    {id:2, name: 'jane doe'}
+  ]
+
   constructor() { }
+
+getUsers(){
+  return of(this.user);
 }
+
+}
+
