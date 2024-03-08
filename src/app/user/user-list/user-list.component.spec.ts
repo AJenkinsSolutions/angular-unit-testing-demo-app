8597be3c822d10a5d01c9af89ed2ce8e8deb0a9c
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-08 08:34:55 -0600

    Add: Html unit test

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -2,6 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
 import { UserService } from '../user.service';
+import { of } from 'rxjs';
+import { trigger } from '@angular/animations';
+import { By } from '@angular/platform-browser';
+
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -30,4 +34,28 @@ describe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve users from the UserService on init', () => {
+    //To start the ngonit lifecycle hook in the unit test. update all the data bindings 
+    fixture.detectChanges();
+    //ASSERTIONS
+    expect(userServiceSpy).toHaveBeenCalled();
+  })
+
+  it('should retreive usres from the userservice when the refresh button is clicked', () => {
+    //This will call the ngOnInIt LifeCycle Hook
+    fixture.detectChanges();
+
+
+    //No information about prior calls
+    userServiceSpy.calls.reset();
+
+    //Simulating ther button being pressed
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null)
+    
+    expect(userServiceSpy).toHaveBeenCalled();
+  
+  })
+
 });
